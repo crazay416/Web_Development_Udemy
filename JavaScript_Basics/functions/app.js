1ae34a3d4c14a54704e9777e3d80a48576b156c1
crazay416 <gisaiah416@gmail.com>
2021-11-23 23:40:48 -0800

    Created functions to practice JS

--- JavaScript_Basics/functions/app.js
+++ JavaScript_Basics/functions/app.js
@@ -25,3 +25,51 @@ const greet1 = (message) =>{
 //greet1("Isaiah")
 
 
+const isSnakeEyes = (num1, num2) => {
+    if(num1 === 1 && num2 === 1){
+        console.log("Snake Eyes!");
+    }
+    else{
+        console.log("Not Snake Eyes!");
+    }
+}
+
+//isSnakeEyes(1,1);
+
+
+const capitalize = (word) => {
+    let newWord = word.replace(word[0], word[0].toUpperCase());
+    return newWord;
+}
+//console.log(capitalize("hello"));
+
+const sumArray = array => {
+    let sum = 0;
+    for(let i = 0; i < array.length; i++){
+        sum += array[i];
+    }
+    return sum;
+}
+
+//let array = [5, 4, 3, 2];
+//console.log(sumArray(array));
+
+
+const returnDay = (numDay) =>{
+    numDay = numDay - 1;
+    const week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    if(numDay < 0 || numDay > 6){
+        return null;
+    }
+    else{
+        return week[numDay];
+    }   
+}
+
+console.log(returnDay(1));
+console.log(returnDay(2));
+console.log(returnDay(3));
+console.log(returnDay(4));
+console.log(returnDay(5));
+console.log(returnDay(6));
+console.log(returnDay(7));
