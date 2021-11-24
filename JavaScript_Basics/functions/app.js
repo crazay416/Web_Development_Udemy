bdf6c973ed188b747556df34b8c3a1b8d924f956
crazay416 <gisaiah416@gmail.com>
2021-11-23 23:03:49 -0800

    Started Functions

---
+++ JavaScript_Basics/functions/app.js
@@ -0,0 +1,27 @@
+function singSong(){
+    console.log("DO")
+    console.log("REI")
+    console.log("ME")
+}
+
+function greet(firstName){
+    console.log(`firstName is: ${firstName.toUpperCase()}`);
+}
+
+
+const add = (x) => {
+    return (x + x)
+}
+
+//console.log(add(5));
+
+//singSong()
+
+
+const greet1 = (message) =>{
+    console.log(`Hey ${message}!`)
+}
+
+//greet1("Isaiah")
+
+
